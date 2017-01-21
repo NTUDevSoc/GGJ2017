34ac714633f40d657bf0c0b5c5b6c99f15e219ba
Elliott.Huge <elliott.huge@gmail.com>
2017-01-21 13:58:56 +0000

    missile update

--- GGJ17/Assets/missileAI.cs
+++ GGJ17/Assets/missileAI.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class missileAI : MonoBehaviour {
+    public Rigidbody2D p1;
+    public Rigidbody2D p2;
+    public GameObject miss;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        miss = (GameObject)Instantiate(Resources.Load("missile"));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        var distance = Vector2.Distance(miss.transform.position, GameObject.Find("player_1").transform.position);
+
+        var distance2 = Vector2.Distance(miss.transform.position, GameObject.Find("player_2").transform.position);
+
+        if (distance <= 50)
+        {
+            Destroy((GameObject)miss);
+        }
+        else if (distance2 <= 50)
+        {
+            Destroy((GameObject)miss);
+        }
+
+
+    }
 }
