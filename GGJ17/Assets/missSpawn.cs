d1df8d640f87fbf3831aec5bfc4929b65ae35ed6
Elliott.Huge <elliott.huge@gmail.com>
2017-01-21 15:28:30 +0000

    newversion

---
+++ GGJ17/Assets/missSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var myPrefab = Resources.Load("missile");
+        Debug.Log(myPrefab);
+        var instance = (GameObject)Instantiate(myPrefab);
+        Debug.Log(instance);
+        Vector2 rand = new Vector2(Random.Range(-200, 200)/100, Random.Range(-200, 200)/100);
+        Instantiate(instance, rand, Quaternion.Euler(0, 0, Random.Range(0,360)));
+
+    }
+}
--- GGJ17/Assets/missileAI.cs
+++ GGJ17/Assets/missileAI.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class missileAI : MonoBehaviour {
-    public Rigidbody2D p1;
-    public Rigidbody2D p2;
-    public GameObject miss;
+    public Rigidbody2D miss;
 
     // Use this for initialization
     void Start () {
-        miss = (GameObject)Instantiate(Resources.Load("missile"));
     }
 	
 	// Update is called once per frame
@@ -18,15 +15,6 @@ public class missileAI : MonoBehaviour {
 
         var distance2 = Vector2.Distance(miss.transform.position, GameObject.Find("player_2").transform.position);
 
-        if (distance <= 50)
-        {
-            Destroy((GameObject)miss);
-        }
-        else if (distance2 <= 50)
-        {
-            Destroy((GameObject)miss);
-        }
-
-
+        Debug.Log(distance + distance2);
     }
 }
