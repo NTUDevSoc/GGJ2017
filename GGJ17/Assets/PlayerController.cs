6760599e855ebd976a7acc874323590b9e74bfb0
MTempents <myckytaker@googlemail.com>
2017-01-21 12:41:18 +0000

    Players can move

---
+++ GGJ17/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public string player;
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis($"Horizontal_{player}");
+        float moveVertical = Input.GetAxis($"Vertical_{player}");
+        rb2d.AddForce(new Vector2(moveHorizontal, moveVertical) * speed);
+    }
+}
---
+++ GGJ17/Assets/player_1_movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_1_movement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal_1");
+        float moveVertical = Input.GetAxis("Vertical_1");
+        rb2d.AddForce(new Vector2(moveHorizontal, moveVertical) * speed);
+    }
+}
