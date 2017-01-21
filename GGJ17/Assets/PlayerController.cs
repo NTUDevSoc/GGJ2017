a40aadd19f538dc146dd898b29b19065d27d75a3
MTempents <myckytaker@googlemail.com>
2017-01-21 13:53:25 +0000

    Players move at a better speed (less like a UFO)

--- GGJ17/Assets/PlayerController.cs
+++ GGJ17/Assets/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public string player;
-    public float speed;
+    public double velocityLimit = 2.5;
+    public float speed = 5;
     private Rigidbody2D rb2d;
 
     void Start()
@@ -17,6 +18,18 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis($"Horizontal_{player}");
         float moveVertical = Input.GetAxis($"Vertical_{player}");
+        
+        if ((rb2d.velocity.x > velocityLimit && moveHorizontal > 0)
+            || (rb2d.velocity.x < (0 - velocityLimit) && moveHorizontal < 0))
+        {
+            moveHorizontal = 0;
+        }
+        if ((rb2d.velocity.y > velocityLimit && moveVertical > 0)
+            || (rb2d.velocity.y < (0 - velocityLimit) && moveVertical < 0))
+        {
+            moveVertical = 0;
+        }
+
         rb2d.AddForce(new Vector2(moveHorizontal, moveVertical) * speed);
     }
 }
