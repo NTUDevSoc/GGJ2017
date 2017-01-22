afb9441e6c009c28585697ca09499fd50247e341
MTempents <myckytaker@googlemail.com>
2017-01-22 14:09:12 +0000

    Changed bulletcontroller code as we can just play with the unity setting. Who care right? unity init, fucking all dependent on prefabs. Who needs to actually code when you can just drag and drop force objects. What is force anyway? somesort of quata behaviour. Who cares. Pace out - 50-0

--- GGJ17/Assets/BulletController.cs
+++ GGJ17/Assets/BulletController.cs
@@ -17,15 +17,22 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("up"))
-        {
-            //    transform.position += transform.forward * 5;
+        //if (Input.GetKey("up"))
+        //{
+        //    //    transform.position += transform.forward * 5;
 
-            Vector3 dirX = transform.right *50;
-            Vector3 dirY = transform.up * 50;
+        //    Vector3 dirX = transform.right *50;
+        //    Vector3 dirY = transform.up * 50;
 
-            rb2d.AddForce(new Vector2(dirX.x, dirY.y));
-        }
+        //    rb2d.AddForce(new Vector2(dirX.x, dirY.y));
+        //}
+
+       // Vector3 nPos = this.transform.rotation.eulerAngles; //* a in this direction
+       // Vector3 calcPos = Vector3.Scale(nPos, new Vector3(1.0f, 1.0f, 1.0f));
+       //// Debug.Log("The resolved pos" + nPos);
+       // //this.transform.position(this.transform/p)
+
+       // transform.Translate(this.transform.position + calcPos);
     }
 
     void OnTriggerEnter2D(Collider2D otherObject)
